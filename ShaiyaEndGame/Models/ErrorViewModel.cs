593cd21fd15e8d28b0734ada69d18ae348b20e23
Loyaltyworx Developer <kevin@loyaltyworx.com>
2019-07-19 08:38:32 -0400

    Add project files.

---
+++ ShaiyaEndGame/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShaiyaEndGame.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
